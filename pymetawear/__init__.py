4e9b5b2408c3303ebb92d982bb304871e56a61aa
Henrik Blidh <henrik.blidh@swedwise.com>
2016-10-13 14:06:07 +0200

    Preparation for 0.5.2 release.

--- pymetawear/__init__.py
+++ pymetawear/__init__.py
@@ -15,9 +15,9 @@ from pymetawear.mbientlab.metawear.functions import setup_libmetawear
 from pymetawear.utils import IS_64_BIT
 
 # Version information.
-__version__ = '0.5.2.dev2'
+__version__ = '0.5.2'
 version = __version__  # backwards compatibility name
-version_info = (0, 5, 2, 'dev2')
+version_info = (0, 5, 2)
 
 if os.environ.get('METAWEAR_LIB_SO_NAME') is not None:
     libmetawear = cdll.LoadLibrary(os.environ["METAWEAR_LIB_SO_NAME"])
