4384bc7e06e2fa62dc651eb449eaf8b191445f89
Marcus Georgi <marcus@kinemic.de>
2017-02-02 15:13:35 +0100

    include sample rate change in sensorfusion example

--- examples/sensorfusion.py
+++ examples/sensorfusion.py
@@ -27,20 +27,28 @@ def handle_quat(data):
     # Handle a (epoch_time, (w,x,y,z)) quaternion tuple.¬
     epoch = data[0]
     xyzaccu = data[1]
-    print("[{0}] W {1}, X {2}, Y {3}, Z {4}".format(epoch, *xyzaccu))
+    print("QUAT [{0}] W {1}, X {2}, Y {3}, Z {4}".format(epoch, *xyzaccu))
 
 def handle_notification(data):
     # Handle a (epoch_time, (x,y,z,accuracy)) corrected acc¬
     # tuple.¬
     epoch = data[0]
     xyzaccu = data[1]
-    print("[{0}] X: {1}, Y: {2}, Z: {3}".format(epoch, *xyzaccu[:-1]))
+    print("ACC [{0}] X: {1}, Y: {2}, Z: {3}".format(epoch, *xyzaccu[:-1]))
+
+def handle_gyro(data):
+    # Handle a (epoch_time, (x,y,z,accuracy)) corrected gyro¬
+    # tuple.¬
+    epoch = data[0]
+    xyzaccu = data[1]
+    print("GYRO [{0}] X: {1}, Y: {2}, Z: {3}".format(epoch, *xyzaccu[:-1]))
+
 
 def handle_euler(data):
     # Handle a (epoch_time, (heading,pitch,roll,yaw)) euler angle tuple.¬
     epoch = data[0]
     xyzaccu = data[1]
-    print("[{0}] Heading: {1}, Pitch: {2}, Roll: {3}, Yaw: {4}".format(
+    print("EULER [{0}] Heading: {1}, Pitch: {2}, Roll: {3}, Yaw: {4}".format(
         epoch, *xyzaccu))
 
 
@@ -48,10 +56,15 @@ print("Write Sensor Fusion settings...")
 c.sensorfusion.set_mode(SensorFusion.MODE_NDOF)
 c.sensorfusion.set_acc_range(SensorFusion.ACC_RANGE_8G)
 c.sensorfusion.set_gyro_range(SensorFusion.GYRO_RANGE_1000DPS)
+print("Set Time Processor to limit data rate to 50Hz for each channel")
+c.sensorfusion.set_sample_delay(SensorFusion.DATA_CORRECTED_ACC, 20)
+c.sensorfusion.set_sample_delay(SensorFusion.DATA_CORRECTED_GYRO, 20)
+c.sensorfusion.set_sample_delay(SensorFusion.DATA_QUATERION, 20)
 print("Subscribing to Sensor Fusion Quaternion signal notifications...")
-c.sensorfusion.notifications(quaternion_callback=handle_quat)
 #c.sensorfusion.notifications(euler_angle_callback=handle_euler)
-#c.sensorfusion.notifications(corrected_acc_callback=handle_notification)
+c.sensorfusion.notifications(corrected_acc_callback=handle_notification,
+                             quaternion_callback=handle_quat,
+                             corrected_gyro_callback=handle_gyro)
 
 time.sleep(20.0)
 
