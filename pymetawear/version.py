b68bf94d61826099396693c3867cca3287cb8d32
Henrik Blidh <henrik.blidh@swedwise.se>
2019-11-01 18:23:12 +0100

    Version bump

--- pymetawear/version.py
+++ pymetawear/version.py
@@ -1,18 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-version.py
------------
-
-Release data for the PyMetaWear project.
-
-:copyright: 2016-11-28 by hbldh <henrik.blidh@nedomkull.com>
-
-"""
 
 from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
-__version__ = '0.11.3a3'
+__version__ = '0.12.0'
 version = __version__  # backwards compatibility name
